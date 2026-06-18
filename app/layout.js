5aeb1e82cf442f37795ad3c2181e5f398ec23d11
umutcff <umidza-map101@code.edu.az>
2026-06-18 14:51:31 +0400

    lab done

--- app/layout.js
+++ app/layout.js
@@ -6,10 +6,15 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
+
+
+
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+
 
 export const metadata = {
   title: "DevForge // dev grid",
